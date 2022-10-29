cca20b3f2cba2ebeaeafad7823bee42c5c88a01c
Dimitrije Jankov <dimitrijejankov@gmail.com>
2022-10-29 05:35:22 +0000

    The library loads fine :)

--- applications/xla_kernel/xla_lib_init.cc
+++ applications/xla_kernel/xla_lib_init.cc
@@ -3,6 +3,7 @@
 #include "../../main/ud_functions/udf_manager.h"
 
 #include "xla_types.h"  
+#include "xla_test_kernel.h"  
 
 
 extern "C" {
@@ -12,15 +13,17 @@ extern "C" {
   }
  
   void register_udfs(bbts::udf_manager_ptr udf_manager) {
-    // udf_manager->register_udf(std::make_unique<bbts::ud_func_t>(
-    //       bbts::ud_func_t {
-    //         .ud_name = "ffnn_act_mult",
-    //         .is_ass = false,
-    //         .is_com = false,
-    //         .num_in = 2,
-    //         .num_out = 1,
-    //         .impls = {}
-    //       }));
-    // udf_manager->register_udf_impl(std::make_unique<bbts::ffnn_activation_mult>());
+    
+    // register the ud fucntion
+    udf_manager->register_udf(std::make_unique<bbts::ud_func_t>(
+        bbts::ud_func_t {
+          .ud_name = "xla_test_kernel",
+          .is_ass = false,
+          .is_com = false,
+          .num_in = 2,
+          .num_out = 1,
+          .impls = {}
+        }));
+    udf_manager->register_udf_impl(std::make_unique<bbts::xla_test_kernel>());
   }
 }
